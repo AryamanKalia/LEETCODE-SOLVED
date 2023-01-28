e3c1843485f6f7e0300206e1cf12bc34c680bdae
AryamanKalia <100133216+AryamanKalia@users.noreply.github.com>
2023-01-28 20:17:00 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (31.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    vector<vector<int> > arr;
+    int solve(int m, int n, int i, int j){
+        if(i==m-1 && j==n-1)    return 1;
+        if(i>m-1 || j>n-1)  return 0;
+        if(arr[i][j]!=-1)    return arr[i][j];
+        return arr[i][j] = solve(m, n, i+1, j) + solve(m, n, i, j+1);
+    }
+    
     int uniquePaths(int m, int n) {
-       return round(tgamma(m+n-2+1)/(tgamma(m)*tgamma(n)));
+        arr.resize(m, vector<int>(n, -1));
+        return solve(m, n, 0, 0);
     }
 };
