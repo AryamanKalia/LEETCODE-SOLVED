0c4b7a5fd75592f0238333a95a9b467e60a08530
AryamanKalia <100133216+AryamanKalia@users.noreply.github.com>
2023-06-14 16:30:40 +0530

    Time: 91 ms (91.57%), Space: 59 MB (32.55%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,33 +1,18 @@
-/*
-    Given array of heights, find max water container can store
-    Ex. height = [1,8,6,2,5,4,8,3,7] -> 49, (8 - 1) x min(8, 7)
-
-    2 pointers outside in, greedily iterate pointer w/ lower height
-
-    Time: O(n)
-    Space: O(1)
-*/
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0;
-        int j = height.size() - 1;
-        
-        int curr = 0;
-        int result = 0;
-        
-        while (i < j) {
-            curr = (j - i) * min(height[i], height[j]);
-            result = max(result, curr);
-            
-            if (height[i] <= height[j]) {
-                i++;
-            } else {
-                j--;
+        int less=0; 
+        int more=height.size()-1;
+        int ans=INT_MIN;
+        while(less<more){
+            ans=max(ans,min(height[less],height[more])*(more-less));
+            if(height[less]<=height[more]){
+                less++;
+            }
+            else if(height[less]>height[more]){
+                more--;
             }
         }
-        
-        return result;
+                    return ans;
     }
 };
