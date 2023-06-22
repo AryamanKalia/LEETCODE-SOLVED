7513d2f78e7ae0f4f083397facddc80d88c9e488
AryamanKalia <100133216+AryamanKalia@users.noreply.github.com>
2023-06-22 19:06:38 +0530

    Time: 459 ms (89.50%), Space: 164.7 MB (94.32%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,91 @@
+#include <unordered_map>
+
+class LRUCache {
+private:
+    struct Node {
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+        
+        Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    };
+    
+    int capacity;
+    std::unordered_map<int, Node*> cache;
+    Node* head;
+    Node* tail;
+
+    void moveToFront(Node* node) {
+        if (node == head) {
+            return;
+        }
+        removeNode(node);
+        addToFront(node);
+    }
+
+    void removeNode(Node* node) {
+        if (node->prev) {
+            node->prev->next = node->next;
+        } else {
+            head = node->next;
+        }
+        if (node->next) {
+            node->next->prev = node->prev;
+        } else {
+            tail = node->prev;
+        }
+    }
+
+    void addToFront(Node* node) {
+        node->next = head;
+        node->prev = nullptr;
+        if (head) {
+            head->prev = node;
+        }
+        head = node;
+        if (!tail) {
+            tail = node;
+        }
+    }
+
+    void removeLast() {
+        if (tail) {
+            cache.erase(tail->key);
+            Node* prev = tail->prev;
+            if (prev) {
+                prev->next = nullptr;
+            } else {
+                head = nullptr;
+            }
+            tail = prev;
+        }
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity), head(nullptr), tail(nullptr) {}
+    
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            moveToFront(node);
+            return node->value;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            moveToFront(node);
+        } else {
+            if (cache.size() >= capacity) {
+                removeLast();
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addToFront(newNode);
+        }
+    }
+};
