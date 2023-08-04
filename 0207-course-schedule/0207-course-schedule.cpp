1c23fab40fcc60c30ae36be730fc37b68b27c004
Aryaman Kalia <100133216+AryamanKalia@users.noreply.github.com>
2023-08-04 23:02:37 +0530

    Time: 15 ms (97.77%), Space: 13.4 MB (78.53%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,42 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, int node, vector<int>& visited, vector<int>& recStack) {
-        visited[node] = 1;
-        recStack[node] = 1;
-        for(int i = 0; i < adj[node].size(); i++) {
-            int nextNode = adj[node][i];
-            if(!visited[nextNode]) {
-                if(dfs(adj, nextNode, visited, recStack)) return true;
-            }
-            else if(recStack[nextNode]) return true;
-        }
-        recStack[node] = 0;
-        return false;
-    }
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(int i = 0; i < prerequisites.size(); i++) {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        // Create an adjacency list to represent the directed graph
+        vector<vector<int>> adj_list(numCourses);
+        // Create an indegree list to keep track of the number of incoming edges for each node
+        vector<int> indegree(numCourses);
+        // Add the edges to the adjacency list and update the indegree list
+        for (auto& p : prerequisites) {
+            adj_list[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
         }
-        vector<int> visited(numCourses);
-        vector<int> recStack(numCourses);
-        for(int i = 0; i < numCourses; i++) {
-            if(!visited[i]) {
-                if(dfs(adj, i, visited, recStack)) return false;
+        // Create a queue to store the nodes with indegree 0
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        // Perform a topological sort
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int neighbor : adj_list[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        // Check if all nodes have been visited (indegree is 0 for all nodes)
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] != 0) {
+                return false;
             }
         }
         return true;
