87aa6ee6b369e1c31d8b0939da9aabf26a69cbbd
Aryaman Kalia <100133216+AryamanKalia@users.noreply.github.com>
2023-08-09 11:44:58 +0530

    Time: 4 ms (28.91%), Space: 7.8 MB (15.17%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  int rec(vector<int>& nums,int idx,vector<int>&dp){
+       if(idx >= nums.size()) return 0;
+       if(dp[idx] != -1) return dp[idx];
+       return dp[idx] = max(rec(nums, idx+1, dp), nums[idx] + rec(nums, idx+2, dp));
+  }
+  int rob(vector<int>& nums) {
+      vector<int>dp(nums.size()+1,-1);
+      return rec(nums,0,dp);
+  }
+};
