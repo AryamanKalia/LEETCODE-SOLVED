08cb7e7ea5d70ff2a79fc5078e36bc57eeeb98b5
Aryaman Kalia <100133216+AryamanKalia@users.noreply.github.com>
2023-09-03 17:59:26 +0530

    Time: 0 ms (100.00%), Space: 16 MB (25.08%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,18 +1,26 @@
+// Recursive Solution
+// Time complexity : O(N*(2^N))
+// Space complexity : O(N*(2^N))
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs;
-        vector<int> sub;
-        subsets(nums, 0, sub, subs);
-        return subs;
-    }
-private:
-    void subsets(vector<int>& nums, int i, vector<int> sub, vector<vector<int>>& subs) {
-        subs.push_back(sub);
-        for (int j = i; j < nums.size(); j++) {
-            sub.push_back(nums[j]);
-            subsets(nums, j + 1, sub, subs);
-            sub.pop_back();
+    vector<vector<int>> ans;
+    
+    void sub(vector<int> &nums, int i, vector<int> temp)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+            return;
         }
+        
+        sub(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        sub(nums, i+1, temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;       
+        sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
+        return ans;
     }
 };
